63da0bb005a42bc10e3be1df4830ed7e9c647c5d
dvmatheussantos <166125263+dvmatheussantos@users.noreply.github.com>
2024-04-09 08:32:04 -0300

    Add files via upload
    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("ola mundo")
+a = 4
+b = 9
+c = a + b
+d = a + b * c
+if a > b :
+    print("este numero e o maior")
+else:
+    print("este não e o maior numero")
+if b > a :
+    print("este numero e o maior")
+else:
+    print("este não e o maior numero")
+
+soma = ("a + b = c")
+multiplicação =("a + b * c")
+print(c)
+print(d)
